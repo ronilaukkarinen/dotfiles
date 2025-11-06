5ddd7dc1132ba77a60eb111b2978e1829d910934
Roni Laukkarinen <roni@dude.fi>
2025-11-06 17:31:26 +0200

    Change variable colors to bright purple

--- nvim/init.lua
+++ nvim/init.lua
@@ -36,10 +36,13 @@ require("lazy").setup("shared.plugins")
 vim.cmd.colorscheme "tokyonight"
 
 -- Apply custom variable colors (override orange with cyan/blue/purple)
-vim.api.nvim_set_hl(0, '@variable', { fg = '#7dcfff' }) -- cyan
+vim.api.nvim_set_hl(0, '@variable', { fg = '#c678dd' }) -- bright purple
 vim.api.nvim_set_hl(0, '@variable.builtin', { fg = '#bb9af7' }) -- purple
 vim.api.nvim_set_hl(0, '@variable.parameter', { fg = '#e0af68' }) -- yellow
 vim.api.nvim_set_hl(0, '@variable.member', { fg = '#73daca' }) -- teal
+-- Bash/shell specific
+vim.api.nvim_set_hl(0, '@variable.bash', { fg = '#c678dd' }) -- bright purple
+vim.api.nvim_set_hl(0, '@constant.bash', { fg = '#c678dd' }) -- bright purple for CONSTANTS
 
 -- Load shared configuration modules
 require('shared.options')
