e7d234c56d7e00697add0e65c1d24a4d49d23aff
Roni Laukkarinen <roni@dude.fi>
2025-11-22 10:16:34 +0200

    Consistent wezterm styles for Linux

--- wezterm/os/linux.lua
+++ wezterm/os/linux.lua
@@ -6,12 +6,22 @@ function M.apply(config)
   -- Linux-specific settings
   config.enable_wayland = true
 
-  -- Font size adjustment for Linux
-  config.font_size = 12
+  config.window_background_opacity = 0.60
 
-  -- Font spacing for Linux
-  config.line_height = 1.0
-  config.cell_width = 0.84
+  config.font_size = 12
+  config.line_height = 1.15
+  config.cell_width = 0.9
+
+  config.window_padding = {
+    left = '3.5cell',
+    right = '3.5cell',
+    top = '2.5cell',
+    bottom = '1.5cell',
+  }
+
+  -- Terminal size
+  config.initial_cols = 87
+  config.initial_rows = 28
 
   -- Disable window decorations on Linux
   -- INTEGRATED_BUTTONS causes rendering glitches
